2c5fe7bc494e8e3d8fe75861dd3630964fdb0fd1
VivyBnuuy <vivybnuuy@gmail.com>
2025-01-29 12:14:20 -0500

    Added functionality to portfolio and close buttons. Added mobile layout for portfolio

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,28 @@
 "use strict";
+
+// Get content element classes
+let portfolioElement = document.getElementsByClassName("portfolio")[0];
+let cardElement = document.getElementsByClassName("contents")[0];
+
+// Get button element classes
+let portfolioButtonElement = document.getElementsByClassName("buttons__portfolio")[0];
+let portfolioCloseEelemnent = document.getElementsByClassName("exit-icon")[0];
+
+function buttonListeners() {
+    portfolioButtonElement.addEventListener("click", showPortfolio);
+    portfolioCloseEelemnent.addEventListener("click", closePortfolio);
+}
+
+function showPortfolio() {
+    cardElement.style = "display: none;";
+
+    // Keeps the portfolio element from flashing onto the screen at the same time as the card is hidden
+    setTimeout(function() {portfolioElement.style = "display: grid;";}, 10);
+}
+
+function closePortfolio() {
+    portfolioElement.style = "display: none;";
+    cardElement.style = "display: grid;";
+}
+
+document.onload = buttonListeners();
