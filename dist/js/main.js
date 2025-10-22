478517f5c9994d430ad06669bfa274c7d9caea41
Vivy Bun <vivy@vivybnuuy.com>
2025-10-22 11:10:34 -0400

    Refactored javascript

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,9 @@
 "use strict";
 
 // Get content element classes
-let portfolioElement = document.getElementsByClassName("portfolio")[0];
-let commissionsElement = document.getElementsByClassName("commissions")[0];
 let cardElement = document.getElementsByClassName("contents")[0];
+let commissionsElement = document.getElementsByClassName("commissions")[0];
+let portfolioElement = document.getElementsByClassName("portfolio")[0];
 
 // Get button element classes
 let commissionsButtonElement = document.getElementsByClassName("buttons__commissions")[0];
@@ -14,37 +14,59 @@ let commissionsCloseElement = document.getElementsByClassName("exit-icon")[0];
 let portfolioCloseElement = document.getElementsByClassName("exit-icon")[1];
 
 function buttonListeners() {
-    // Portfolio
-    portfolioButtonElement.addEventListener("click", showPortfolio);
-    portfolioCloseElement.addEventListener("click", closePortfolio);
-
     // Commissions
-    commissionsButtonElement.addEventListener("click", showCommissions);
-    commissionsCloseElement.addEventListener("click", closeCommissions);
+    commissionsButtonElement.addEventListener("click", showTarget);
+    commissionsCloseElement.addEventListener("click", closeTarget);
+
+    // Portfolio
+    portfolioButtonElement.addEventListener("click", showTarget);
+    portfolioCloseElement.addEventListener("click", closeTarget);
 }
 
-function showCommissions() {
-    cardElement.style = "display: none;";
+// Shows website sections depending on which button is clicked
+function showTarget(source) {
+    let clickedElement = source.currentTarget;
 
-    // Keeps the portfolio element from flashing onto the screen at the same time as the card is hidden
-    setTimeout(function() {commissionsElement.style = "display: grid;";}, 10);
+    switch(true) {
+        // Commissions
+        case (clickedElement.className == "buttons buttons__commissions"):
+            cardElement.style.animationName = "fade-out";
+
+            setTimeout(function() {
+                cardElement.style = "display: none;";
+                commissionsElement.style = "display: grid";
+                commissionsElement.style.animationName = "fade-in";
+            }, 500);
+            break;
+
+        // Portfolio
+        case (clickedElement.className == "buttons buttons__portfolio"):
+            cardElement.style.animationName = "fade-out";
+
+            setTimeout(function() {
+                cardElement.style = "display: none;";
+                portfolioElement.style = "display: grid";
+                portfolioElement.style.animationName = "fade-in";
+            }, 500);
+    }
 }
 
-function showPortfolio() {
-    cardElement.style = "display: none;";
+// Closes open website section and goes back to main contents
+function closeTarget(source) {
+    let clickedElement = source.currentTarget;
 
-    // Keeps the portfolio element from flashing onto the screen at the same time as the card is hidden
-    setTimeout(function() {portfolioElement.style = "display: grid;";}, 10);
-}
+    switch(true) {
+        // Commissions
+        case (clickedElement.className == "commissions-card__info__links exit-icon"):
+            commissionsElement.style = "display: none;";
+            cardElement.style = "display: grid;";
+            break;
 
-function closeCommissions() {
-    commissionsElement.style = "display: none;";
-    cardElement.style = "display: grid;";
-}
-
-function closePortfolio() {
-    portfolioElement.style = "display: none;";
-    cardElement.style = "display: grid;";
+        // Portfolio    
+        case (clickedElement.className == "portfolio-card__info__links exit-icon"):
+            portfolioElement.style = "display: none;";
+            cardElement.style = "display: grid;";
+    }
 }
 
 document.onload = buttonListeners();
